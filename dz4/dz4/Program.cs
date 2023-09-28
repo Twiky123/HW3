e53cd73b10c8683bb32b48b62321371cf7b1b311
Oleg <tvikster4@gmail.com>
2023-09-28 11:02:01 +0300

    \\lp

---
+++ dz4/dz4/Program.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz4
+{
+    internal class Program
+    {
+        enum days_of_the_week 
+        {
+            Подпевельник,
+            Вторни,
+            Срида,
+            Щитверг,
+            Пьянтица,
+            Суеббота,
+            Воскрещение
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задача 1");
+
+            int[] Sequence = new int[10];
+            bool result = true;
+            Random randNum = new Random();
+
+            Console.Write("Получилась последовательность: ");
+
+            for (int i = 0; i < Sequence.Length; i++)
+            {
+                Sequence[i] = randNum.Next(50);
+                Console.Write(Sequence[i] + " ");
+            }
+
+            for (int i = 0; i < (Sequence.Length - 1); i++)
+            {
+                if (Sequence[i] > Sequence[i + 1])
+                {
+                    Console.WriteLine($"\nПоследовательность не является возрастающей. Число {Sequence[i + 1]} нарушающает последовательнось, число под номером {i + 2}");
+                    result = false;
+                    break;
+                }
+            }
+
+            if (result)
+            {
+                Console.WriteLine("\nПоследовательнось является возрастающей");
+            }
+
+
+            Console.WriteLine("Задача 2");
+            try
+            {
+                byte cardNum;
+
+                Console.Write("Введите номер карты (число от 6 до 14): ");
+                cardNum = byte.Parse(Console.ReadLine());
+
+                switch (cardNum)
+                {
+                    case 6:
+                        Console.WriteLine("Вы ввели 6 - это Шестерка");
+                        break;
+                    case 7:
+                        Console.WriteLine("Вы ввели 7 - это Семерка");
+                        break;
+                    case 8:
+                        Console.WriteLine("Вы ввели 8 - это Восьмерка");
+                        break;
+                    case 9:
+                        Console.WriteLine("Вы ввели 9 - это Девятка");
+                        break;
+                    case 10:
+                        Console.WriteLine("Вы ввели 10 - это Десятка");
+                        break;
+                    case 11:
+                        Console.WriteLine("Вы ввели 11 - это Валет");
+                        break;
+                    case 12:
+                        Console.WriteLine("Вы ввели 12 - это Дама");
+                        break;
+                    case 13:
+                        Console.WriteLine("Вы ввели 13 - это Король");
+                        break;
+                    case 14:
+                        Console.WriteLine("Вы ввели 14 - это Туз");
+                        break;
+                    default:
+                        throw new Exception("Вы ввели число меньше 6 или больше 14");
+                }
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine("Вы ввели не число или число является дробным (Необходимо ввести целое число от 6 до 14)");
+            }
+            catch (System.OverflowException)
+            {
+                Console.WriteLine("Вы ввели не число или число является дробным (Необходимо ввести целое число от 6 до 14)");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+
+            Console.WriteLine("Задача 3");
+            string String;
+            Console.WriteLine("Подсказка: \n" +
+                              "Jabroni - Patron Tequila\n" +
+                              "School Counselor - Anything with Alcohol\n" +
+                              "Programmer - Hipster Craft Beer\n" +
+                              "Bike Gang Members - Moonshine\n" +
+                              "Politician - Your tax dollars\n" +
+                              "Rapper - Cristal\n");
+
+            Console.Write("Введите сообщение: ");
+            String = Console.ReadLine().ToLower();
+
+            switch (String)
+            {
+                case "jabroni":
+                    Console.WriteLine("Patron Tequila");
+                    break;
+                case "school counselor":
+                    Console.WriteLine("Anything with Alcohol");
+                    break;
+                case "programmer":
+                    Console.WriteLine("Hipster Craft Beer");
+                    break;
+                case "bike gang member":
+                    Console.WriteLine("Moonshine");
+                    break;
+                case "politician":
+                    Console.WriteLine("Your tax dollars");
+                    break;
+                case "rapper":
+                    Console.WriteLine("Cristal");
+                    break;
+                default:
+                    Console.WriteLine("Beer");
+                    break;
+            }
+            Console.WriteLine("Задача 4");
+
+            try
+            {
+                byte day;
+
+                Console.Write("Введите число дня недели: ");
+                day = byte.Parse(Console.ReadLine());
+
+                switch (day)
+                {
+                    case 1:
+                        days_of_the_week monday = days_of_the_week.Подпевельник;
+                        Console.WriteLine($"Вы ввели {day} - это {monday}");
+                        break;
+                    case 2:
+                        days_of_the_week tuesday = days_of_the_week.Вторни;
+                        Console.WriteLine($"Вы ввели {day} - это {tuesday}");
+                        break;
+                    case 3:
+                        days_of_the_week wednesday = days_of_the_week.Срида;
+                        Console.WriteLine($"Вы ввели {day} - это {wednesday}");
+                        break;
+                    case 4:
+                        days_of_the_week thursday = days_of_the_week.Щитверг;
+                        Console.WriteLine($"Вы ввели {day} - это {thursday}");
+                        break;
+                    case 5:
+                        days_of_the_week friday = days_of_the_week.Пьянтица;
+                        Console.WriteLine($"Вы ввели {day} - это {friday}");
+                        break;
+                    case 6:
+                        days_of_the_week saturday = days_of_the_week.Суеббота;
+                        Console.WriteLine($"Вы ввели {day} - это {saturday}");
+                        break;
+                    case 7:
+                        days_of_the_week sunday = days_of_the_week.Воскрещение;
+                        Console.WriteLine($"Вы ввели {day} - это {sunday}");
+                        break;
+                    default:
+                        Console.WriteLine("Вы ввели цифру меньше 1 или больше 7");
+                        break;
+                }
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine("Необходимо ввести цифру дня от 1 до 7");
+            }
+            catch (System.OverflowException)
+            {
+                Console.WriteLine("Вы ввели слишком большое или слишком маленькое число (Необходимо ввести цифру от 1 до 7)");
+            }
+
+
+            Console.WriteLine("Задача 5");
+
+            string[] toys = { "Hello Kitty", "Barbie doll" };
+            int count = 0;
+
+            Console.Write("Строка: ");
+
+            for (int i = 0; i < toys.Length; i++)
+            {
+                Console.Write(toys[i] + ", ");
+
+                string toys1 = toys[i].ToLower();
+
+                if ((toys1 == "hello kitty") || (toys1 == "barbie doll"))
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine($"В сумке лежит {count} кукол");
+        }
+    }
+}
---
+++ labatymakova_akalaba4/labatymakova_akalaba4/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labatymakova_akalaba4
+{
+    internal class Program
+    {
+        enum Monthname
+        {
+            Янаврь,
+            Феврь,
+            Мрт,
+            Апрль,
+            Май,
+            Июн,
+            Июл,
+            Агуст,
+            Сентбрь,
+            Октбрь,
+            Нобрь,
+            Декбрь
+        }
+
+        static void Main(string[] args)
+        
+        {
+            Console.WriteLine("Упражнение 4.1/4.2");//Метод напролом(в тупую)
+            try
+            {
+                Console.Write("Введите число от 1 до 365 (номер дня в году):");
+                int day = int.Parse(Console.ReadLine());
+                if ((day < 1) || (day > 365))
+                {
+                    throw new Exception("Вы ввели число меньше 1 или больше 365");
+                }
+                DateTime data  = new DateTime(2023, 1, 1).AddDays(day - 1);
+                switch (data.Month)
+                {
+                    case 01:
+                        Console.WriteLine($"{data.Day} Января");
+                        break;
+                    case 02:
+                        Console.WriteLine($"{data.Day} Февраля");
+                        break;
+                    case 03:
+                        Console.WriteLine($"{data.Day} Марта");
+                        break;
+                    case 04:
+                        Console.WriteLine($"{data.Day} Апреля");
+                        break;
+                    case 05:
+                        Console.WriteLine($"{data.Day} Мая");
+                        break;
+                    case 06:
+                        Console.WriteLine($"{data.Day} Июня");
+                        break;
+                    case 07:
+                        Console.WriteLine($"{data.Day} Июля");
+                        break;
+                    case 08:
+                        Console.WriteLine($"{data.Day} Августа");
+                        break;
+                    case 09:
+                        Console.WriteLine($"{data.Day} Сентября");
+                        break;
+                    case 10:
+                        Console.WriteLine($"{data.Day} Октября");
+                        break;
+                    case 11:
+                        Console.WriteLine($"{data.Day} Ноября");
+                        break;
+                    case 12:
+                        Console.WriteLine($"{data.Day} Декабря");
+                        break;
+                }
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine("Вы ввели не число или число дробное");
+            }
+            catch (Exception dateException)
+            {
+                Console.WriteLine(dateException.Message);
+            }
+            Console.WriteLine("Домашняя задача 4.1"); //Метод не напролом(не в тупую)
+            try
+            {
+                int day;
+                Console.Write("Введите год:");
+                int yaer = int.Parse(Console.ReadLine());
+                bool leap = yaer > -46 && (yaer % 400 == 0 || //Проверка на високосный год
+                yaer % 100 != 0 && yaer % 4 == 0) || yaer == -46;
+                if (leap == true)
+                {
+                    Console.Write("Введите число от 1 до 366 (номер дня в году):");
+                    day = int.Parse(Console.ReadLine());
+                    if ((day < 1) || (day > 366))
+                    {
+                        throw new Exception("Вы ввели число меньше 1 или больше 366");
+                    }
+                    int[] months = new int[] { 31, leap ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+                    Monthname[] name = (Monthname[])Enum.GetValues(typeof(Monthname));
+
+                    int i;
+                    for (i = 0; day > months[i] && i < 12; i++) day -= months[i];
+                    Console.WriteLine($"{day} {name[i]}");
+
+                }
+                else
+                {
+                    Console.Write("Введите число от 1 до 365 (номер дня в году):");
+                    day = int.Parse(Console.ReadLine());
+                    if ((day < 1) || (day > 365))
+                    {
+                        throw new Exception("Вы ввели число меньше 1 или больше 365");
+                    }
+                    int[] months = new int[] { 31, leap ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+                    Monthname[] name = (Monthname[])Enum.GetValues(typeof(Monthname));
+
+                    int i;
+                    for (i = 0; day > months[i] && i < 12; i++) day -= months[i];
+                    Console.WriteLine($"{day} {name[i]}");
+                }
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine("Вы ввели не число или число дробное");
+            }
+            catch (Exception dateException)
+            {
+                Console.WriteLine(dateException.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
